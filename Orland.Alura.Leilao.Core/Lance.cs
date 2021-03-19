cf516b908e2832e144e0e85a1923f4f10eb7829b
Iuri <iuri.orlandi20@gmail.com>
2021-03-19 11:49:48 -0300

    Iniciando projeto.

---
+++ Orland.Alura.Leilao.Core/Lance.cs
@@ -0,0 +1,14 @@
+﻿namespace Orland.Alura.Leilao.Core
+{
+    public class Lance
+    {
+        public Interessada Cliente { get; }
+        public double Valor { get; }
+
+        public Lance(Interessada cliente, double valor)
+        {
+            Cliente = cliente;
+            Valor = valor;
+        }
+    }
+}
